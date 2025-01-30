b19d529fb8f174e7a21da56cf179729a3cc41d22
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-01-30 23:41:02 +0530

    added navigation in header using useNavigate hook

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import styles from "./header.module.css";
+import { useNavigate } from "react-router-dom";
 const { header, navbarList, navbarListItem } = styles;
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate("/");
+  };
+
+  const goToUser = () => {
+    navigate("/user");
+  };
+
+  const goToAddUser = () => {
+    navigate("/add-user");
+  };
+
+  const goToEditUser = () => {
+    navigate("/edit-user");
+  };
+
   return (
     <>
       <header className={header}>
         <nav>
           <ul className={navbarList}>
-            <li className={navbarListItem}>Home</li>
-            <li className={navbarListItem}>User Profile</li>
-            <li className={navbarListItem}>Add User</li>
-            <li className={navbarListItem}>Edit User</li>
+            <li className={navbarListItem} onClick={goToHome}>
+              Home
+            </li>
+            <li className={navbarListItem} onClick={goToUser}>
+              User Profile
+            </li>
+            <li className={navbarListItem} onClick={goToAddUser}>
+              Add User
+            </li>
+            <li className={navbarListItem} onClick={goToEditUser}>
+              Edit User
+            </li>
           </ul>
         </nav>
       </header>
