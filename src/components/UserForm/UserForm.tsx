121eeb461243da14bde8efe6e221a68fc886a910
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-02-03 19:11:30 +0530

    adding fields, styling forms, success message, navigation home after user creation

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 import ErrorComponent from "../Error/ErrorComponent";
 import Loader from "../Loader/Loader";
 import { User } from "../../types/userTypes";
+import styles from "./userForm.module.css";
+import { useNavigate } from "react-router-dom";
+
+const { formContainer, successMessageStyle, inputField, button } = styles;
 
 interface UserFormProps {
   clickedUser: User | undefined;
 }
 
-type UserFormData = Pick<User, "name" | "email">;
+type UserFormData = Pick<User, "name" | "email" | "phone" | "website">;
 
 const UserForm: React.FC<UserFormProps> = ({ clickedUser }) => {
+  const navigate = useNavigate();
   const [user, setUser] = useState<UserFormData>({
     name: !clickedUser ? "" : clickedUser.name,
     email: !clickedUser ? "" : clickedUser.email,
+    phone: clickedUser ? clickedUser.phone : "",
+    website: clickedUser ? clickedUser.website : "",
   });
   const [error, setError] = useState<null | string>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -27,7 +34,7 @@ const UserForm: React.FC<UserFormProps> = ({ clickedUser }) => {
     try {
       setIsLoading(true);
       const response = await fetch(
-        `https://jsonplaceholder.typicode.com/users`,
+        `https://jsonplaceholder.typicode.com/usershh`,
         {
           method: "POST",
           headers: {
@@ -41,11 +48,16 @@ const UserForm: React.FC<UserFormProps> = ({ clickedUser }) => {
       }
       const createdUser = await response.json();
       console.log(createdUser);
-      setsuccessMessage(`User with ${createdUser.name} created successfully!`);
-      setUser({ name: "", email: "" });
+      setsuccessMessage(
+        `User with ${createdUser.name} ${
+          clickedUser ? "Updated" : "Created"
+        } successfully!`
+      );
+      setUser({ name: "", email: "", phone: "", website: "" });
 
       setTimeout(() => {
         setsuccessMessage(null);
+        navigate("/");
       }, 1000);
     } catch (error) {
       setError((error as Error).message);
@@ -57,19 +69,22 @@ const UserForm: React.FC<UserFormProps> = ({ clickedUser }) => {
   if (error) return <ErrorComponent message={error} />;
 
   return (
-    <>
+    <div className={formContainer}>
       {isLoading ? (
         <Loader />
       ) : (
         <>
           <h1>User Form</h1>
-          {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+          {successMessage && (
+            <p className={successMessageStyle}>{successMessage}</p>
+          )}
           <input
             type="text"
             placeholder="Your Full Name..."
             name="name"
             onChange={changeHandler}
             value={user.name}
+            className={inputField}
           />
           <input
             type="email"
@@ -77,13 +92,30 @@ const UserForm: React.FC<UserFormProps> = ({ clickedUser }) => {
             name="email"
             onChange={changeHandler}
             value={user.email}
+            className={inputField}
           />
-          <button onClick={submitHandler}>
+          <input
+            type="text"
+            placeholder="Your Phone..."
+            name="phone"
+            onChange={changeHandler}
+            value={user.phone}
+            className={inputField}
+          />
+          <input
+            type="text"
+            placeholder="Your Website..."
+            name="website"
+            onChange={changeHandler}
+            value={user.website}
+            className={inputField}
+          />
+          <button className={button} onClick={submitHandler}>
             {clickedUser ? "Update" : "Add"} User
           </button>
         </>
       )}
-    </>
+    </div>
   );
 };
 
