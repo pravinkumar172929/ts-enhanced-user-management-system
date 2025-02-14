4fb70d113ef4a03d0815100700d956fca64961f1
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-02-14 12:56:45 +0530

    installed useForm and tested it

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./Login.module.css";
+import { useForm } from "react-hook-form";
 const { formContainer, inputField, button } = styles;
 
 interface UserInputProps {
@@ -8,7 +9,12 @@ interface UserInputProps {
   password: string;
 }
 
+interface TestFormInputs {
+  test: string;
+}
+
 const Login: React.FC = () => {
+  const { register, handleSubmit } = useForm<TestFormInputs>();
   const authContext = useContext(AuthContext);
   if (!authContext) {
     throw new Error("Something wrong!!");
@@ -27,6 +33,10 @@ const Login: React.FC = () => {
   const loginHandler = async (e: React.FormEvent) => {
     e.preventDefault();
     await login(userInput.email, userInput.password);
+  };
+
+  const onSubmit = (data: TestFormInputs) => {
+    console.log("submittedData => ", data.test);
   };
 
   return (
@@ -51,6 +61,10 @@ const Login: React.FC = () => {
       <button onClick={loginHandler} className={button}>
         Login
       </button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("test")} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
