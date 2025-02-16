7c3bdaf4071039d0f2b28e4c081cacd541f4a7c7
Pravin Kumar <pravinkumar29.1729@gmail.com>
2025-02-16 12:55:22 +0530

    removed commented code and modified loginHandler

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./Login.module.css";
 import * as yup from "yup";
